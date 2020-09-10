26990b25d39a5f0475fc53b602ae2c97b8ed3722
Martin Kagamino Lehoux <martin@lehoux.net>
2020-09-10 10:32:02 +0200

    fix - Remove unused

--- app.js
+++ app.js
@@ -10,25 +10,6 @@ const config = require("./config.json");
 const clientRouter = require("./controllers/client");
 const produitRouter = require("./controllers/produit");
 
-function search(word, users) {
-  word = word.toLowerCase();
-  const result = [];
-  users.forEach(user => {
-    if (
-      (user.nom && user.nom.toLowerCase().includes(word)) ||
-      (user.prenom && user.prenom.toLowerCase().includes(word)) ||
-      (user.surnom && user.surnom.toLowerCase().includes(word))
-    ) result.push(user);
-    else if (word.split(" ").every(part => {
-      return (
-        (user.nom && user.nom.toLowerCase().includes(part)) ||
-        (user.prenom && user.prenom.toLowerCase().includes(part)) ||
-        (user.surnom && user.surnom.toLowerCase().includes(part))
-      )
-    })) result.push(user);
-  });
-  return result;
-}
 
 const app = express();
 const store = new mongoDBStore({
